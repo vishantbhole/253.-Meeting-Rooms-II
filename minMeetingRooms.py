cd1ecd24477709194299be61a79cf6c4fe3dda5d
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-25 21:26:57 -0600

    Update minMeetingRooms.py

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -25,3 +25,8 @@ class Solution:
                 count -= 1
             output = max(output, count)
         return output
+        
+if __name__ == "__main__":
+    sol = Solution()
+    intervals = [Interval(0, 40), Interval(5, 10), Interval(15, 20)]
+    print("Output is:", sol.minMeetingRooms(intervals))  # Expected False [web:2][web:4][web:6]
