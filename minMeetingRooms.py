3a8d9fa81ce450f1a971311187019565580a9687
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-25 21:25:59 -0600

    Update minMeetingRooms.py

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -10,3 +10,8 @@ class Interval(object):
         
 class Solution:
     def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        count, output = 0, 0
+        s, e = 0, 0
