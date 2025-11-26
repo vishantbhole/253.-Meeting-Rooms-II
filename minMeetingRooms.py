72d46d7fdd8c75ed62012f57f5c53391739676e3
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-25 21:25:06 -0600

    Update minMeetingRooms.py

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -1,2 +1,9 @@
 # 253. Meeting Rooms II
 
+#Definition of Interval:
+from typing import List
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
