eef204302250a3c1c1030e658ab9f0d5e1a781cb
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-25 21:25:34 -0600

    Update minMeetingRooms.py

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -7,3 +7,6 @@ class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+        
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
